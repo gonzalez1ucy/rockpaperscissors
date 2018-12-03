0a231fc517335ff07b1cf1eb4bcf29ee4d4aeff8
Luzmarie Gonzalez <glucy822@gmail.com>
2018-12-03 18:33:37 +0000

    okay

--- js/script.js
+++ js/script.js
@@ -21,26 +21,36 @@ $("#shoot").click(function(){
           computerChoice = "scissors";
    }
        $("#computerChoice").text(computerChoice);
-  if (computerChoice ==="rock" && userChoice === "paper"){
-  $("#result").text("userChoice Wins!");
-} if (computerChoice ==="paper" && userChoice === "paper"){
-  $("#result").text("Draw!");
-} if (computerChoice ==="rock" && userChoice === "rock"){
-  $("#result").text("Draw!");
-} if (computerChoice ==="paper" && userChoice === "rock"){
-  $("#result").text("computerChoice Wins!");
-} if (computerChoice ==="scissors" && userChoice === "paper"){
-  $("#result").text("computerChoice Wins!");
-} if (computerChoice ==="scissors" && userChoice === "rock"){
-  $("#result").text("userChoice Wins!");
-} if (computerChoice ==="scissors" && userChoice === "scissors"){
-  $("#result").text("Draw!");
-} if (computerChoice ==="paper" && userChoice === "scissors"){
-  $("#result").text("userChoice Wins!");
-} if (computerChoice ==="rock" && userChoice === "paper"){
-  $("#result").text("userChoice Wins!");
-} if (computerChoice ==="rock" && userChoice === "scissors"){
-  $("#result").text("computerChoice Wins!");
+  
+if (computerChoice ==="rock" && userChoice === 'paper'){
+    $("#result").text("user Wins!");
+} 
+if (computerChoice ==="paper" && userChoice === 'paper'){
+    $("#result").text("Draw!");
+} 
+if (computerChoice ==="rock" && userChoice === 'rock'){
+    $("#result").text("Draw!");
+} 
+if (computerChoice ==="paper" && userChoice === 'rock'){
+    $("#result").text("computer Wins!");
+} 
+if (computerChoice ==="scissors" && userChoice === 'paper'){
+   $("#result").text("computer Wins!");
+}
+if (computerChoice ==="scissors" && userChoice === 'rock'){
+    $("#result").text("user Wins!");
+}
+if (computerChoice ==="scissors" && userChoice === 'scissors'){
+    $("#result").text("Draw!");
+} 
+if (computerChoice ==='paper' && userChoice === 'scissors'){
+    $("#result").text("user Wins!");
+}
+if (computerChoice ==='rock' && userChoice === 'paper'){
+    $("#result").text("user Wins!");
+} 
+if (computerChoice ==='rock' && userChoice === 'scissors'){
+    $("#result").text("computer Wins!");
 } 
 });
  
